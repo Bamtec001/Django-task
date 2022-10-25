9da9a00ed281d2a399568c9611bf9688de054db5
Aderiye <92952476+Bamtec001@users.noreply.github.com>
2022-10-25 14:04:45 +0100

    Update models.py

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from turtle import title
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
